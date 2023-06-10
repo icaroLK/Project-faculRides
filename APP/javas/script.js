2e2562c3290d0e24ab94c81890b1b4e2b68ee1b9
Ícaro Kuchanovicz <icarolima.k7@gmail.com>
2023-06-10 17:33:36 -0300

    nice

--- APP/javas/script.js
+++ APP/javas/script.js
@@ -13,21 +13,38 @@ function GoToPage(go_to){
 
 function progressBar(go_to, qtd){
     var progressFillx = document.getElementById("progressFill"+(go_to));
-    var pageWidth = go_to * (100/qtd);
+    if(go_to==1){
+        var pageWidth = 1;
     progressFillx.style.width = pageWidth + '%';
+    }else{
+
+        var pageWidth = (go_to-1) * (100/qtd);
+        progressFillx.style.width = pageWidth + '%';
+    }
 }
 
 
 var lig = false;
 function onoffajuda(){
     var ajuda = document.getElementById('ajuda');
+    var reenv = document.getElementById('reenv');
     if(lig==false){
         ajuda.style.display = "block";
+        reenv.style.marginTop = 'calc(100vh - 490px)';
         lig = true;
     }else if(lig==true){
         ajuda.style.display = 'none';
+        reenv.style.marginTop = 'calc(100vh - 400px)';
         lig = false;
     }
 
 
 }
+
+
+function jumpToNextInput(atual) {
+    var nextInput = document.getElementById('icod' + (atual + 1));
+    if (nextInput) {
+      nextInput.focus();
+    }
+  }
